4f3c4ad56ab3ca7262d8cbea23bb9057c7d63661
Graham Christensen <graham.christensen@iamgraham.net>
2010-10-29 14:48:05 -0400

    Initial commit.

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    sananton
+ * @subpackage form
+ * @author     Graham Christensen 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
