f0b53d16339271c4213ddbea508ef3684f5c695c
Graham Christensen <graham.christensen@iamgraham.net>
2010-10-29 19:01:38 -0400

    Tweaking some on the form. Getting it to look better, etc.

--- apps/frontend/modules/person/templates/newSuccess.php
+++ apps/frontend/modules/person/templates/newSuccess.php
@@ -1,8 +1,17 @@
+<h1 class="add_listing">Add Yourself</h1>
+
+<p>*All fields are required.</p>
+<form action="/members" class="new_member" enctype="multipart/form-data" id="new_member" method="post">
+
 <?php echo $form->renderHiddenFields(); ?>
 <?php foreach ($form as $field_name => $field) { if ($field->isHidden()) { continue; }?>
       <p>
         <?php echo $field->renderLabel(); ?>
-        <?php echo $field->render(); ?>
+        <?php echo $field->render(array('size' => 30)); ?>
         <span class="hint"><?php echo $form->getWidgetSchema()->getHelp($field_name); ?></span>
     </p>
 <?php } ?>
+<p class="submit">
+    <input id="member_submit" name="commit" type="submit" value="Submit" />
+  </p>
+</form>
--- lib/form/PersonForm.class.php
+++ lib/form/PersonForm.class.php
@@ -13,17 +13,18 @@ class PersonForm extends BasePersonForm
   {
       $help = array(
           'name' => 'Who are you?',
-          'email' => 'Required to activate your account. Not shown, sold, or spammed.',
+          'email' => 'Required to activate your account.<br />Not shown, sold, or spammed.',
           'website' => 'You have one... right?',
           'image' => 'Image will be cropped t o a 200x200 square.',
-          'person_category_list' => 'What are you?'
+          'person_category_list' => "Choose <strong>up to 3</strong> categories.<br />Don't see yours? "
+                                    . '<a href="' . sfConfig::get('app_help_link') . '" target="blank">Let me know.</a>'
           );
       foreach ($help as $w => $h) {
           $this->widgetSchema->setHelp($w, $h);
       }
       
       $this->widgetSchema->setLabel('person_category_list', 'What are you?');
-        
+      $this->getWidget('person_category_list')->setOption('expanded', true);
       $this->useFields(array('name', 'website', 'email', 'image', 'person_category_list'));
   }
 }
