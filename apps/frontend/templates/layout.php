5139f477ab03e0215eefea780a3e7742293507ac
Vid Luther <vid@zippykid.com>
2010-10-31 16:32:21 -0500

    Changed list yourself to add yourself

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -29,11 +29,10 @@
 
         <div id="footer"> 
             <div id="footer-inner"> 
-                <p class="footer-links"><?php echo link_to('Get Listed', '@person_new'); ?> | <?php echo link_to('Edit Your Listing', '@person_edit_request'); ?></p> 
+                <p class="footer-links"><?php echo link_to('+ Add Yourself', '@person_new'); ?> | <?php echo link_to('Edit Your Listing', '@person_edit_request'); ?></p> 
                 <p class="byline">
                     Developed by <?php echo link_to('Graham Christensen', 'http://grahamc.com/', array('target' => '_blank')); ?>
-                    inspired by <?php echo link_to('Travis Roberts', 'http://travisonrails.com/', array('target' => '_blank')); ?>
-                    By the way, I'm on <?php echo link_to('Github', 'http://github.com/grahamc/sananton'); ?>.
+                    on <?php echo link_to('Github', 'http://github.com/grahamc/sananton'); ?>.
 				
                 </p>
             </div>
