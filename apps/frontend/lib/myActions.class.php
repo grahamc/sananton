93e9f6eb8e65d552b9e21ec76182d0309f1249bf
Graham Christensen <graham.christensen@iamgraham.net>
2010-11-01 10:40:16 -0400

    Adding custom methods to the user for has-based authentication.

--- apps/frontend/lib/myActions.class.php
+++ apps/frontend/lib/myActions.class.php
@@ -1,5 +1,22 @@
 <?php
 abstract class myActions extends sfActions {
+    /**
+     * Sign in if possible
+     */
+    public function preExecute() {
+        $r = $this->getRequest();
+        if ($r->hasParameter('hash')) {
+            $hash = $r->getParameter();
+            $ph = PersonHashPeer::retrieveByPk($hash);
+            if ($ph instanceof PersonHash && $ph->isValid()) {
+                $this->getUser()->signIn($ph);
+            } else {
+                $this->getUser()->setFlash('Sorry, that hash has been used.');
+                $this->redirect('@homepage');
+            }
+        }
+    }
+    
     public function forward404If($condition, $message = null) {
         if ($condition) {
             $this->getUser()->setFlash('error', $message);
--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,55 @@
 
 class myUser extends sfBasicSecurityUser
 {
+    /**
+     * Log in as a hash if the hash is valid
+     * @return bool
+     */
+    public function signIn(PersonHash $hash) {
+        if (!$hash->isValid()) {
+            return false;
+        }
+        
+        $this->hash = $hash;
+        $this->setAttribute('hash', $hash->getHash());
+        $this->setAuthenticated(true);
+        
+        return true;
+    }
+    
+    /**
+     * Deauthenticate and invalidate the auth token
+     */
+    public function signOut() {
+        // Invalidate the token
+        $this->hash->makeUsed();
+        
+        // Remove their information
+        $this->hash = null;
+        $this->setAttribute('hash', null);
+        $this->setAuthenticated(false);
+    }
+    
+    /**
+     * Get the Person record currently logged in
+     * @return Person|false[if not logged in]
+    public function getPerson() {
+        if ($this->isAuthenticated()) {
+            return false;
+        }
+        
+        return $this->getHash()->getPerson();
+    }
+    
+    /**
+     * Get the PersonHash object which is currently logged in
+     * @return Personhash|falsep[if not logged in]
+     */
+    public function getHash() {
+        if ($this->isAuthenticated()) {
+            return false;
+        }
+        
+        return $this->hash;
+    }
 }
