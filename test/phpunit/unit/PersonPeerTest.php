30f79b32ebb7747e520f13cbcc7218e6fae113c6
Graham Christensen <graham.christensen@iamgraham.net>
2010-11-02 11:07:22 -0400

    Testing the PersonPeer model.

---
+++ test/phpunit/unit/PersonPeerTest.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'BaseTestModel.php';
+
+class unit_PersonPeerTest extends BaseTestModel
+{
+  public function testGetByEmail()
+  {
+      $email = 'graham@grahamc.com';
+      
+      $m = PersonPeer::getByEmail($email);
+      
+      $this->assertTrue($m instanceof Person, 'Result should be a Person object');
+      $this->assertEquals($email, $m->getEmail(), 'The email shoudl match the requested email.');
+  }
+  
+  public function testGetActiveByPage() {
+      $r1 = PersonPeer::getActiveByPage(1, 1);
+      $this->assertEquals(1, count($r1), 'There should be a single result');
+      
+      $r2 = PersonPeer::getActiveByPage(2, 1);
+      $this->assertEquals(1, count($r1), 'There should be a single result');
+      
+      $this->assertNotEquals($r1, $r2, 'The two results should be different pages of data.');
+  }
+  
+  public function testGetActive() {
+      $ps = PersonPeer::getActive();
+      $this->assertEquals(2, count($ps), 'There should only be two active users.');
+      
+      foreach ($ps as $p) {
+          $this->assertTrue($p instanceof Person, 'All results should be a Person object.');
+      }
+  }
+  
+  public function testCountActive() {
+      $this->assertEquals(2, PersonPeer::countActive(), 'Starts at 2 active users');
+      
+      $this->createInactiveUser();
+      $this->assertEquals(2, PersonPeer::countActive(), 'Adding another inactive user should not increase the number.');
+  }
+  
+  protected function createInactiveUser() {
+      $p = new Person();
+      $p->setName('foo');
+      $p->save();
+      
+      return $p;
+  }
+}
