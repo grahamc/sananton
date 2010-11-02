a0c7b72c89fdd494d43fc9eb0b5052534419a099
Graham Christensen <graham.christensen@iamgraham.net>
2010-11-02 08:45:37 -0400

    Adding the basics for phpunit and a sample test.

---
+++ test/phpunit/unit/strtolowerTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__).'/../bootstrap/unit.php';
+
+class unit_strtolowerTest extends sfPHPUnitBaseTestCase
+{
+  public function testDefault()
+  {
+      $this->assertEquals('foo', strtolower('FOO'));
+  }
+}
